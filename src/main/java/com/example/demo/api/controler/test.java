a680f96ddb8a512b77e2438dae863e62b782c27f
Quy Nguyen Phu <QuyNP5@fpt.com>
2024-06-27 17:22:05 +0700

    ok

--- src/main/java/com/example/demo/api/controler/test.java
+++ src/main/java/com/example/demo/api/controler/test.java
@@ -1,5 +1,6 @@
 package com.example.demo.api.controler;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api")
 public class test {
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping(value = { "/list" })
     public String productDto() {
 
